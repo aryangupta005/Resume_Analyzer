1c082f90d513cc39909cfc60d6f554cc2410057a
GDGouravDey <gdgouravdey@gmail.com>
2024-08-02 19:54:42 +0530

    Small Update

--- resumeparser.py
+++ resumeparser.py
@@ -24,19 +24,24 @@ def ats_extractor(resume_data):
     You are an AI trained to extract information from resumes. Given a resume, identify and return the following details in JSON format:
 
     1. Full Name
-    2. Email Address
+    2. Email Address and Phone Number(an extra 'pe' is coming at the start in some cases. Ignore irrelevant 'pe' at start of email address.)
     3. GitHub Profile URL
     4. LinkedIn Profile URL
-    5. Education Details (degree, institution, dates)
-    6. Employment History (company, position, dates)
-    7. Project Details in Short
+    5. Education Details (Degree, Institution, Dates, Percentage / CGPA [Only include those details which are given in the resume])
+    6. Employment History (Company, Position, Dates)
+    7. Project Details in Short (Ignore the word 'Demo', it is used to indicate Demo Link)
     8. Certifications
     9. Achievements and Awards
     10. Technical Skills
     11. Soft Skills
     12. Other Relevant Details
     
-    Note:- In case of email address, an extra 'pe' is coming at the start in some cases. Ignore irrelevant 'pe' before email address. 
+    Extract the skills and relevant keywords to recommend the best job roles for the candidate. Give only the top 3 most matching job roles.
+    
+    Recommend any certifications or courses that the candidate can take to improve their skills. Give maximum of 5 recommendations.
+    
+    Note:- Just give a JSON file, no need to give any explanation. Remove extra 'pe' from email address. Do not need to number anything.
+    
     """
 
     response = model.generate_content(prompt + resume_data)
@@ -45,5 +50,10 @@ def ats_extractor(resume_data):
         response_text = response_text[7:].strip()  
     if response_text.endswith("```"):
         response_text = response_text[:-3].strip()
-    print(response.text)
-    return response_text
+    print(response_text)
+    return remove_empty_brackets(response_text)
+
+def remove_empty_brackets(input_string):
+    lines = input_string.splitlines()
+    filtered_lines = [line for line in lines if '[]' not in line]
+    return '\n'.join(filtered_lines)
