7327bec49492e1aa266b2b6c2d03a610674860b1
GDGouravDey <gdgouravdey@gmail.com>
2024-08-03 18:03:14 +0530

    Structure Update

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-import google.generativeai as genai
-import json
 import os
+import sys
+import json
+from flask import Flask, request, render_template
+from pypdf import PdfReader
+import google.generativeai as genai
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
+# Configure the UPLOAD_PATH for saving uploaded files
+UPLOAD_PATH = r"__DATA__"
+app = Flask(__name__)
+
+# Configure the path for custom modules
+sys.path.insert(0, os.path.abspath(os.getcwd()))
+
+# Load the GEMINI API key
 api_key = os.environ.get('GEMINI_API_KEY')
 
 # Check if API key is loaded
@@ -73,3 +85,48 @@ def remove_empty_brackets(input_string):
     lines = input_string.splitlines()
     filtered_lines = [line for line in lines if '[]' not in line]
     return '\n'.join(filtered_lines)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/process", methods=["POST"])
+def ats():
+    if 'pdf_doc' not in request.files:
+        return render_template('index.html', error="No PDF file uploaded. Please upload a valid PDF.")
+
+    doc = request.files['pdf_doc']
+    
+    if doc.filename == '':
+        return render_template('index.html', error="No file selected. Please select a PDF file to upload.")
+
+    # Save the uploaded PDF file
+    doc.save(os.path.join(UPLOAD_PATH, "file.pdf"))
+    doc_path = os.path.join(UPLOAD_PATH, "file.pdf")
+    
+    # Read the file and extract information
+    data = _read_file_from_path(doc_path)
+    json_data = ats_extractor(data)
+    
+    try:
+        json_object = json.loads(json_data)
+    except ValueError as e:
+        print("Invalid JSON response:", e)
+        json_object = {}
+
+    print("Processed Data:", json_object)
+    
+    return render_template('index.html', data=json.dumps(json_object))
+
+def _read_file_from_path(path):
+    reader = PdfReader(path) 
+    data = ""
+
+    for page_no in range(len(reader.pages)):
+        page = reader.pages[page_no] 
+        data += page.extract_text()
+
+    return data 
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
